5ee5f2e7a28f87d1e8135d9594911b323290b935
pujilaksoo608 <pujilaksono608@gmail.com>
2023-12-19 21:59:52 +0700

    [FEATURE] ADD migration

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 import os
 from src.config.config import Config
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 load_dotenv()
 
@@ -10,3 +12,13 @@ app = Flask(__name__)
 config = Config().dev_config
 
 app.env = config.ENV
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI_DEV")
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+from src.models.user_model import User
